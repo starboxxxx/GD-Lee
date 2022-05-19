0a6c0e9c010d96720803aae35a23641b10ee7162
starboxxxx <98813748+starboxxxx@users.noreply.github.com>
2022-05-19 14:29:30 +0900

    Update file.py

--- file.py
+++ file.py
@@ -1,61 +1,35 @@
-file = open('Fullmeta_20220428162419.txt', "r", encoding="euc-kr")
+import file_manage as fm
+import setting as set
 
-dict = {'cat_id' : 'cat_name'}
-error = {'cat_id' : 'cat_name'}
+DATA_FILE, CAT_META, ERR_META = fm.pathRead()
+file = open(DATA_FILE, "r", encoding="euc-kr")
 
+# 데이터 파일에서 가져온 값들 중 조건에 충족하는 값을 넣을 딕셔너리 생성
+dict = {}
+
+# 값은 같지만 키가 다른 예외들을 넣을 딕셔너리 생성
+error = {}
+
+A,B = set.headerName(file, dict, error)
 while True:
     data1 = file.readline()
     if not data1:
         break
     data2 = data1.split('\t')
-    try:
-        Category_fullname2 = data2[31]
-        Category_menu_code2 = data2[55]
-    except:
-        continue
+
+    # 위에서 지정한 헤더 명이 'Category_fullname'과 'Category_menu_code'인 필드의 인덱스 번호를 활용하여 해당하는 필드 값을 한줄 씩 저장
+    Category_fullname2 = data2[A]
+    Category_menu_code2 = data2[B]
+
+    # 구분자를 '>'를 이용해 값들을 리스트 형태로 정렬
     Category_fullname = Category_fullname2.split('>')
     Category_menu_code = Category_menu_code2.split('>')
 
-    if Category_fullname[0] == '키즈랜드':
-        s = len(Category_menu_code)
-        if s > 4:
-            for i in range(1, 4):
-                if Category_menu_code[i] in dict:
-                    if Category_fullname[i] != dict[Category_menu_code[i]]:
-                        error[Category_menu_code[i]] = Category_fullname[i]
-                    else:
-                        continue
-                else:
-                    dict[Category_menu_code[i]] = Category_fullname[i]
-        elif s <= 4:
-            if Category_menu_code[s-1].startswith("CV"):
-                for i in range(1, s-1):
-                    if Category_menu_code[i] in dict:
-                        if Category_fullname[i] != dict[Category_menu_code[i]]:
-                            error[Category_menu_code[i]] = Category_fullname[i]
-                        else:
-                            continue
-                    else:
-                        dict[Category_menu_code[i]] = Category_fullname[i]
-            else:
-                for i in range(1, s):
-                    if Category_menu_code[i] in dict:
-                        if Category_fullname[i] != dict[Category_menu_code[i]]:
-                            error[Category_menu_code[i]] = Category_fullname[i]
-                        else:
-                            continue
-                    else:
-                        dict[Category_menu_code[i]] = Category_fullname[i]
-    else:
-        continue
+    # 0 depth 값이 값이 키즈랜드인지 아닌지 확인
+    set.depthSetting(Category_menu_code, Category_fullname, dict, error)
 
-
-with open('category_meta.txt', 'w', encoding='UTF-8') as f:
-    for cat_id,cat_name in dict.items():
-        f.write(f"{cat_id} \t {cat_name}\n")
-
-with open('cat_id_key_err.txt', 'w', encoding='UTF-8') as f:
-    for err_id, err_name in error.items():
-        f.write(f"{err_id} \t {err_name}\n")
+# 메타파일 생성
+fm.makeFile(CAT_META,"cat_id","cat_name", dict)
+fm.makeFile(ERR_META,"err_id","err_name", error)
 
 file.close()
